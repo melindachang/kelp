41adcdc2d57135367813f233e263bb4126c345f6
Melinda Chang <melindachang.hy@gmail.com>
2025-08-26 18:06:11 -0400

    feat: prettier setup, reset styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import '../lib/styles/main.scss'
+</script>
+
+<svelte:head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
