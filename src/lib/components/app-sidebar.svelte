8d2cb5e5348f5409a156d45a2a2357309344305e
Melinda Chang <melindachang.hy@gmail.com>
2025-08-29 16:26:45 -0400

    feat(shadcn)!: overhaul layout to component sidebar-16

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,46 +1,111 @@
-<script lang="ts">
-  import type { ComponentProps } from 'svelte'
-
-  import * as Sidebar from '$lib/components/ui/sidebar'
-  import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
-  import * as Collapsible from '$lib/components/ui/collapsible'
-
-  import ModeToggle from '$lib/components/mode-toggle.svelte'
-
+<script lang="ts" module>
   import {
-    Shrimp,
-    House,
-    Heart,
-    LayoutDashboard,
-    ChevronDown,
-    Ellipsis,
-    BookOpen,
-    FolderKanban
+    SquareTerminalIcon,
+    BotIcon,
+    BookOpenIcon,
+    Settings2Icon,
+    LifeBuoyIcon,
+    SendIcon,
+    FilmIcon,
+    MusicIcon,
+    TvIcon,
+    ShrimpIcon
   } from '@lucide/svelte'
 
-  const menu_items = [
-    { title: 'Home', url: '/', icon: House },
-    { title: 'Favorites', url: '/favorites', icon: Heart },
-    { title: 'Dashboard', url: '/dashboard', icon: LayoutDashboard }
-  ]
-
-  const libraries = [
-    { title: 'Movies', url: '/libraries/movies' },
-    { title: 'Music', url: '/libraries/music' }
-  ]
-
-  let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props()
+  const data = {
+    user: {
+      name: 'shadcn',
+      email: 'm@example.com',
+      avatar: '/avatars/shadcn.jpg'
+    },
+    navMain: [
+      {
+        title: 'Playground',
+        url: '#',
+        icon: SquareTerminalIcon,
+        isActive: true,
+        items: [
+          { title: 'History', url: '#' },
+          { title: 'Starred', url: '#' },
+          { title: 'Settings', url: '#' }
+        ]
+      },
+      {
+        title: 'Models',
+        url: '#',
+        icon: BotIcon,
+        items: [
+          { title: 'Genesis', url: '#' },
+          { title: 'Explorer', url: '#' },
+          { title: 'Quantum', url: '#' }
+        ]
+      },
+      {
+        title: 'Documentation',
+        url: '#',
+        icon: BookOpenIcon,
+        items: [
+          { title: 'Introduction', url: '#' },
+          { title: 'Get Started', url: '#' },
+          { title: 'Tutorials', url: '#' },
+          { title: 'Changelog', url: '#' }
+        ]
+      },
+      {
+        title: 'Settings',
+        url: '#',
+        icon: Settings2Icon,
+        items: [
+          { title: 'General', url: '#' },
+          { title: 'Team', url: '#' },
+          { title: 'Billing', url: '#' },
+          { title: 'Limits', url: '#' }
+        ]
+      }
+    ],
+    navSecondary: [
+      { title: 'Support', url: '#', icon: LifeBuoyIcon },
+      { title: 'Feedback', url: '#', icon: SendIcon }
+    ],
+    libraries: [
+      { name: 'Movies', url: '#', icon: FilmIcon },
+      { name: 'Music', url: '#', icon: MusicIcon },
+      { name: 'Shows', url: '#', icon: TvIcon }
+    ]
+  }
 </script>
 
-<Sidebar.Root collapsible="offcanvas" {...restProps}>
+<script lang="ts">
+  import type { ComponentProps } from 'svelte'
+  import * as Sidebar from '$lib/components/ui/sidebar/index.js'
+  import NavMain from './nav-main.svelte'
+  import NavLibraries from './nav-libraries.svelte'
+  import NavSecondary from './nav-secondary.svelte'
+  import NavUser from './nav-user.svelte'
+
+  let {
+    ref = $bindable(null),
+    ...restProps
+  }: ComponentProps<typeof Sidebar.Root> = $props()
+</script>
+
+<Sidebar.Root
+  class="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+  {...restProps}>
   <Sidebar.Header>
     <Sidebar.Menu>
       <Sidebar.MenuItem>
-        <Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:!p-1.5">
+        <Sidebar.MenuButton size="lg">
           {#snippet child({ props })}
-            <a href="/" {...props}>
-              <Shrimp class="!size-5" />
-              <span class="text-base font-semibold">Kelp</span>
+            <a href="##" {...props}>
+              <div
+                class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <ShrimpIcon class="size-4" />
+              </div>
+              <div class="grid flex-1 text-left text-sm leading-tight">
+                <span class="truncate font-medium">Kelp</span>
+                <span class="truncate text-xs">v0.1.0</span>
+              </div>
             </a>
           {/snippet}
         </Sidebar.MenuButton>
@@ -48,74 +113,11 @@
     </Sidebar.Menu>
   </Sidebar.Header>
   <Sidebar.Content>
-    <Sidebar.Group>
-      <Sidebar.GroupContent>
-        <Sidebar.Input id="search" placeholder="Search..." />
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
-    <Sidebar.Group>
-      <Sidebar.GroupLabel>General</Sidebar.GroupLabel>
-      <Sidebar.GroupContent>
-        <Sidebar.Menu>
-          {#each menu_items as item (item.title)}
-            <Sidebar.MenuItem>
-              <Sidebar.MenuButton>
-                {#snippet child({ props })}
-                  <a href={item.url} {...props}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </a>
-                {/snippet}
-              </Sidebar.MenuButton>
-            </Sidebar.MenuItem>
-          {/each}
-        </Sidebar.Menu>
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
-    <Sidebar.Group>
-      <Sidebar.GroupLabel>Media</Sidebar.GroupLabel>
-      <Sidebar.GroupContent>
-        <Sidebar.Menu>
-          <Collapsible.Root open class="group/collapsible">
-            <Sidebar.MenuItem>
-              <Collapsible.Trigger>
-                {#snippet child({ props })}
-                  <Sidebar.MenuButton {...props}>
-                    <BookOpen />
-                    <span>Libraries</span>
-                    <ChevronDown
-                      class="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                  </Sidebar.MenuButton>
-                {/snippet}
-              </Collapsible.Trigger>
-              <Collapsible.Content>
-                <Sidebar.MenuSub>
-                  {#each libraries as item}
-                    <Sidebar.MenuSubItem>
-                      <Sidebar.MenuSubButton>
-                        {#snippet child({ props })}
-                          <a href={item.url} {...props}>
-                            <span>{item.title}</span>
-                          </a>
-                        {/snippet}
-                      </Sidebar.MenuSubButton>
-                    </Sidebar.MenuSubItem>
-                  {/each}
-                </Sidebar.MenuSub>
-              </Collapsible.Content>
-            </Sidebar.MenuItem>
-          </Collapsible.Root>
-          <Sidebar.MenuItem>
-            <Sidebar.MenuButton>
-              <FolderKanban />
-              <span>Metadata Manager</span>
-            </Sidebar.MenuButton>
-          </Sidebar.MenuItem>
-        </Sidebar.Menu>
-      </Sidebar.GroupContent>
-    </Sidebar.Group>
+    <NavMain items={data.navMain} />
+    <NavLibraries libraries={data.libraries} />
+    <NavSecondary items={data.navSecondary} class="mt-auto" />
   </Sidebar.Content>
   <Sidebar.Footer>
-    <ModeToggle />
+    <NavUser user={data.user} />
   </Sidebar.Footer>
 </Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import '../app.css'
 
-  import * as Sidebar from '$lib/components/ui/sidebar'
   import AppSidebar from '$lib/components/app-sidebar.svelte'
+  import SiteHeader from '$lib/components/site-header.svelte'
+  import * as Sidebar from '$lib/components/ui/sidebar'
 
   import { ModeWatcher } from 'mode-watcher'
 
@@ -11,11 +12,14 @@
 
 <ModeWatcher />
 
-<Sidebar.Provider>
-  <AppSidebar />
-
-  <main>
-    <Sidebar.Trigger />
-    {@render children?.()}
-  </main>
-</Sidebar.Provider>
+<div class="[--header-height:calc(--spacing(14))]">
+  <Sidebar.Provider class="flex flex-col">
+    <SiteHeader />
+    <div class="flex flex-1">
+      <AppSidebar />
+      <Sidebar.Inset>
+        <main>{@render children()}</main>
+      </Sidebar.Inset>
+    </div>
+  </Sidebar.Provider>
+</div>
